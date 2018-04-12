09f71b3342508742524834a93117e7cd96331857
Radoslav Radoev <rradoev@gmail.com>
2018-04-12 00:44:55 -0700

    setup project in eclipse and created the MVC package structure

---
+++ Eclipse-Dev/AmazonLite/src/com/amazonlite/model/Item.java
@@ -0,0 +1,18 @@
+package com.amazonlite.model;
+
+import java.util.Date;
+
+public abstract class Item {
+	
+	private String title;
+	private String author;
+	private String length;
+	private Date released;
+	private ItemType itemType;
+	
+	
+	public Item() {
+		
+	}
+
+}
---
+++ Eclipse-Dev/AmazonLite/src/com/amazonlite/model/ItemType.java
@@ -0,0 +1,5 @@
+package com.amazonlite.model;
+
+public enum ItemType {
+	CD, DVD, BOOK;
+}
