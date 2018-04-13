d98f38fafcfa831ef129a2b9cc10165be327acf3
Radoslav Radoev <rradoev@gmail.com>
2018-04-13 00:36:24 -0700

    Initializing Properties files and generating defaults if don't exist

--- Eclipse-Dev/AmazonLite/src/com/amazonlite/props/InitializeProperties.java
+++ Eclipse-Dev/AmazonLite/src/com/amazonlite/props/InitializeProperties.java
@@ -6,16 +6,27 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Date;
 import java.util.Properties;
 import java.io.OutputStream;
-import java.sql.Date;
 
 import com.amazonlite.model.ItemType;
 
 public class InitializeProperties {
+	
+	public static void main(String[] args) {
+		
+		InitializeProperties initProps = new InitializeProperties();
+		initProps.initializeDefaultProperites(ItemType.CD);
+	}
 
 	private void initializeDefaultProperites(ItemType itemType) {
-		if (checkPropertiesFileExists(itemType)) { }
+		if (checkPropertiesFileExists(itemType)) { 
+			loadProperties(itemType);
+		}
+		else {
+			createDummyProperties(itemType);
+		}
 	}
 	
 	private void createDummyProperties(ItemType itemType) {
@@ -24,39 +35,42 @@ public class InitializeProperties {
 		String fileName = itemType.name() + ".prpperties";
 		
 		try (OutputStream output = new FileOutputStream(fileName)) {
-			if (itemType == itemType.CD) {
+			if (itemType == ItemType.CD) {
 				prop.setProperty("1", 
-						String.format("Title: %s%nAuthor: %s%nLength: %2d%nRelease Date: %tD%nItem Type: %s%n", 
-								"T.N.T", "AC/DC", 41.55, Date.valueOf("12/01/1975"), itemType.name()));
+						String.format("Title: %s,Author: %s,Length: %.2f,Release Date: %tD,Item Type: %s", 
+								"T.N.T", "AC/DC", 41.55D, Date.parse("12/01/1975"), itemType.name()));
 				prop.setProperty("2", 
-						String.format("Title: %s%nAuthor: %s%nLength: %2d%nRelease Date: %tD%nItem Type: %s%n", 
-								"Let There Be Rock", "AC/DC", 40.19, Date.valueOf("03/21/1977"), itemType.name()));
+						String.format("Title: %s,Author: %s,Length: %.2f,Release Date: %tD,Item Type: %s", 
+								"Let There Be Rock", "AC/DC", 40.19D, Date.parse("03/21/1977"), itemType.name()));
 				prop.setProperty("3", 
-						String.format("Title: %s%nAuthor: %s%nLength: %2d%nRelease Date: %tD%nItem Type: %s%n", 
-								"Black Ice", "AC/DC", 55.38, Date.valueOf("10/17/2008"), itemType.name()));
+						String.format("Title: %s,Author: %s,Length: %.2f,Release Date: %tD,Item Type: %s", 
+								"Black Ice", "AC/DC", 55.38D, Date.parse("10/17/2008"), itemType.name()));
 			}
-			else if (itemType == itemType.DVD) {
+			else if (itemType == ItemType.DVD) {
 				prop.setProperty("1", 
-						String.format("Title: %s%nAuthor: %s%nLength: %2d%nRelease Date: %tD%nItem Type: %s%n", 
-								"The Fellowship of the Ring", "Peter Jackson", 3.28, Date.valueOf("12/19/2001"), itemType.name()));
+						String.format("Title: %s,Author: %s,Length: %.2f,Release Date: %tD,Item Type: %s", 
+								"The Fellowship of the Ring", "Peter Jackson", 3.28D, Date.parse("12/19/2001"), itemType.name()));
 				prop.setProperty("2", 
-						String.format("Title: %s%nAuthor: %s%nLength: %2d%nRelease Date: %tD%nItem Type: %s%n", 
-								"The Two Towers", "Peter Jackson", 3.43, Date.valueOf("12/18/2002"), itemType.name()));
+						String.format("Title: %s,Author: %s,Length: %.2f,Release Date: %tD,Item Type: %s", 
+								"The Two Towers", "Peter Jackson", 3.43D, Date.parse("12/18/2002"), itemType.name()));
 				prop.setProperty("3", 
-						String.format("Title: %s%nAuthor: %s%nLength: %2d%nRelease Date: %tD%nItem Type: %s%n", 
-								"The Return of the King", "Peter Jackson", 4.12, Date.valueOf("12/17/2003"), itemType.name()));
+						String.format("Title: %s,Author: %s,Length: %.2f,Release Date: %tD,Item Type: %s", 
+								"The Return of the King", "Peter Jackson", 4.12D, Date.parse("12/17/2003"), itemType.name()));
 			}
-			else if (itemType == itemType.BOOK) {
+			else if (itemType == ItemType.BOOK) {
 				prop.setProperty("1", 
-						String.format("Title: %s%nAuthor: %s%nLength: %2d%nRelease Date: %tD%nItem Type: %s%n", 
-								"The Philosopher's Stone", "J. K. Rowling", 329, Date.valueOf("09/01/1997"), itemType.name()));
+						String.format("Title: %s,Author: %s,Length: %.2f,Release Date: %tD,Item Type: %s", 
+								"The Philosopher's Stone", "J. K. Rowling", 329, Date.parse("09/01/1997"), itemType.name()));
 				prop.setProperty("2", 
-						String.format("Title: %s%nAuthor: %s%nLength: %2d%nRelease Date: %tD%nItem Type: %s%n", 
-								"The Chamber of Secrets", "J. K. Rowling", 341, Date.valueOf("06/02/1998"), itemType.name()));
+						String.format("Title: %s,Author: %s,Length: %.2f,Release Date: %tD,Item Type: %s", 
+								"The Chamber of Secrets", "J. K. Rowling", 341, Date.parse("06/02/1998"), itemType.name()));
 				prop.setProperty("3", 
-						String.format("Title: %s%nAuthor: %s%nLength: %2d%nRelease Date: %tD%nItem Type: %s%n", 
-								"The Prisoner of Azkaban", "J. K. Rowling", 435, Date.valueOf("09/08/1999"), itemType.name()));
+						String.format("Title: %s,Author: %s,Length: %.2f,Release Date: %tD,Item Type: %s", 
+								"The Prisoner of Azkaban", "J. K. Rowling", 435, Date.parse("09/08/1999"), itemType.name()));
 			}
+			
+			prop.store(output, null);
+			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
