86a6a84a1a431a53e2356d0d18dcf0bc36979de6
Radoslav Radoev <rradoev@gmail.com>
2018-05-04 23:40:09 -0700

    Working on the View GUI

--- Eclipse-Dev/AmazonLiteGUI/src/com/amazonlite/View/ViewGUI.java
+++ Eclipse-Dev/AmazonLiteGUI/src/com/amazonlite/View/ViewGUI.java
@@ -1,7 +1,13 @@
 package com.amazonlite.View;
 
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
 import javax.swing.JRadioButton;
+import javax.swing.JSeparator;
+import javax.swing.SwingConstants;
+import javax.swing.border.TitledBorder;
 
 import oracle.jrockit.jfr.JFR;
 
@@ -9,10 +15,20 @@ import javax.swing.JPanel;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.GroupLayout.Group;
+
+import javafx.scene.layout.Border;
+
 import javax.swing.JFrame;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ViewGUI extends JFrame {
 
@@ -31,11 +47,14 @@ public class ViewGUI extends JFrame {
 	
 	private final JPanel mainJPanel;
 	
-	private GroupLayout groupLayout;
+	private BorderLayout layout;
 	
 	public ViewGUI() {
 	
-		mainJPanel = new JPanel();
+		setTitle("Inventory");
+		layout = new BorderLayout();
+		
+		mainJPanel = new JPanel(layout);
 		
 		// Setup the item type radio buttons
 		cdRadioBtn = new JRadioButton("CD");
@@ -54,9 +73,20 @@ public class ViewGUI extends JFrame {
 		itemTypeJPanel.add(dvdRadioBtn);
 		itemTypeJPanel.add(bookRadioBtn);
 		
-		mainJPanel.add(itemTypeJPanel);
+		// Set up items JPanel layout
+		itemTypeJPanel.setLayout(new BoxLayout(itemTypeJPanel, BoxLayout.PAGE_AXIS));
+		itemTypeJPanel.add(Box.createVerticalGlue());
+		itemTypeJPanel.add(Box.createRigidArea(new Dimension(0, 20)));
 		
-
+		// Set title and border on the items JPanel
+		TitledBorder itemsTitle;
+		itemsTitle = BorderFactory.createTitledBorder("Items");
+		itemsTitle.setTitleJustification(TitledBorder.CENTER);
+		itemTypeJPanel.setBorder(itemsTitle);
+		
+		// add the item Jpanel to the main JPanel		
+		mainJPanel.add(itemTypeJPanel, layout.PAGE_START);
+		
 		// Setup action radio buttons
 		addRadioBtn = new JRadioButton("Add item");
 		updateRadioBtn = new JRadioButton("Update item");
@@ -77,43 +107,56 @@ public class ViewGUI extends JFrame {
 		actionsJPanel.add(updateRadioBtn);
 		actionsJPanel.add(searchRadioBtn);
 		actionsJPanel.add(displayRadioBtn);
+
+		// Set actions JPanel layout
+		actionsJPanel.setLayout(new BoxLayout(actionsJPanel, BoxLayout.PAGE_AXIS));
+		actionsJPanel.add(Box.createVerticalGlue());
 		
-		mainJPanel.add(actionsJPanel);
+		// Set title and border on the actions JPanel
+		TitledBorder actionsTitle;
+		actionsTitle = BorderFactory.createTitledBorder("Actions");
+		actionsTitle.setTitleJustification(TitledBorder.CENTER);
+		actionsJPanel.setBorder(actionsTitle);
 		
+		// Disable all actions until an Item has been selected
+		actionsJPanel.setEnabled(false);
+		for (Component component : getComponents(actionsJPanel))
+			component.setEnabled(false);
 		
-		// setting the JPanel layout
-		groupLayout = new GroupLayout(mainJPanel);
-		mainJPanel.setLayout(groupLayout);
+		mainJPanel.add(actionsJPanel, layout.CENTER);
 		
-		// automatic gap insertion
-		groupLayout.setAutoCreateContainerGaps(true);
-		groupLayout.setAutoCreateGaps(true);
-		
-		groupLayout.setHorizontalGroup(
-			groupLayout.createSequentialGroup()
-			.addGroup(createParallelGroup(GroupLayout.Alignment.LEADING))
-				.addComponent(itemTypeJPanel)
-				.addComponent(actionsJPanel));
-		
-		groupLayout.setVerticalGroup(
-				groupLayout.createSequentialGroup()
-				.addGroup(createParallelGroup(GroupLayout.Alignment.BASELINE))
-					.addComponent(itemTypeJPanel)
-					.addComponent(actionsJPanel));
-				
+		pack();
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		add(mainJPanel);
+	}
+	
+	/**
+	 * Method that all components in a specific container
+	 * @param container container to check for and return components
+	 * @return Component(s) contained in the container
+	 */
+	private Component[]	getComponents(Component container) {
+		ArrayList<Component> list = null;
 		
+		// Get every component in the container and add it to the array
+		try {
+			list = new ArrayList<Component>(Arrays.asList(
+					((Container)container).getComponents()));
+			for (int index = 0; index < list.size(); index++) {
+				for (Component currentComponent : getComponents(list.get(index))) {
+					list.add(currentComponent);
+				}
+			}
+		} catch (ClassCastException e) {
+			list = new ArrayList<Component>();
+		}
+		
+		return list.toArray(new Component[list.size()]);
 	}
  	
-	private Group createParallelGroup(Alignment leading) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 	public static void main(String[] args) {
 		ViewGUI vg = new ViewGUI();
 		vg.setVisible(true);
-		vg.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		vg.setSize(200,300);
 	}
 	
