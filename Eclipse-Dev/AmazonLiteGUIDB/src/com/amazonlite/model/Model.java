c6e2c2b2dc4f4d3ddbc545a7baaa1fd19047f796
Radoslav Radoev <rradoev@gmail.com>
2018-05-21 17:22:51 -0700

    Started wokring on the DB Model for Assignment 3

--- Eclipse-Dev/AmazonLiteGUIDB/src/com/amazonlite/model/Model.java
+++ Eclipse-Dev/AmazonLiteGUIDB/src/com/amazonlite/model/Model.java
@@ -4,6 +4,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Properties;
 
@@ -29,11 +30,32 @@ public class Model {
 		// Connect to DB 
 		try {
 		connection = DriverManager.getConnection(dburl, user, pass);
-		System.out.println("DB connection successful to: " + connection);
+		System.out.println("DB connection successful to: " + connection.getSchema());
 		} catch (SQLException sqle) {
 			sqle.printStackTrace();
 		}
 		
+	}
+	
+	public boolean addInventoryItem(String inventoryType) {
+		boolean successful = false;
+		
+		PreparedStatement statement = null;
+		String sql = "INSERT INTO CD (title, author, length, release_date, hit_single)" +
+				" values (?, ?, ?, ?, ?)";
+		
+		try {
+			statement = connection.prepareStatement(sql);
+			statement.setString(1, "test title");
+			statement.setString(2, "test author");
+			statement.setString(3, "5.5");
+			statement.setString(4, "05/05/2005");
+			statement.setString(5, "test hit single");
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		return successful;
 	}
 	
 	public static void main(String[] args) {
